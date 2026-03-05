32e95a9b27ba207bac5f4bf34fd40c59cc7ee52e
Rifky <rifkyyudistiansyah@gmail.com>
2026-03-05 12:47:47 +0700

    added assets and scripts

---
+++ sql_scripts/BalancePerCustomer_SP.sql
@@ -0,0 +1,28 @@
+CREATE PROCEDURE BalancePerCustomer
+    @name VARCHAR(255)
+AS
+BEGIN
+    SELECT 
+        c.CustomerName,
+        a.AccountType,
+        a.Balance,
+        (a.Balance + ISNULL(SUM(
+            CASE 
+                WHEN t.TransactionType = 'Deposit' THEN t.Amount 
+                ELSE -t.Amount 
+            END
+        ), 0)) AS CurrentBalance
+    FROM 
+        DimCustomer c
+    JOIN 
+        DimAccount a ON c.CustomerID = a.CustomerID
+    LEFT JOIN 
+        FactTransaction t ON a.AccountID = t.AccountID
+    WHERE 
+        c.CustomerName = @name 
+        AND a.Status = 'active'
+    GROUP BY 
+        c.CustomerName, 
+        a.AccountType, 
+        a.Balance;
+END;
